550b3777b6ac6f7575c8183b89bc4abf7e452e49
Shasha Sami <shasha.sami91199@gmail.com>
2020-05-27 22:46:42 +0530

    Update kmeans.py

--- kmeans.py
+++ kmeans.py
@@ -1,11 +1,11 @@
 #K-means Clustering of IRIS Dataset
 
 import numpy
-from sklearn import datasets, metrics
+from sklearn import datasets,metrics
 from sklearn.cluster import KMeans
 
 #load data
-(data, targets)=datasets.load_iris(return_X_y=True); 
+(data, targets)= datasets.load_iris(return_X_y=True); 
 
 #create an instance of the model
 model=KMeans(n_clusters=4)
@@ -14,11 +14,11 @@ model=KMeans(n_clusters=4)
 model.fit(data)
 
 #cluster numbers
-prediction = model.labels_
+prediction =model.labels_
 
 
 print(prediction)
 
-#prediction = numpy.choose(prediction, [0,1,2]).astype(numpy.int64)
+#prediction =numpy.choose(prediction, [0,1,2]).astype(numpy.int64)
 
 #print(prediction)
